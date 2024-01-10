aaf2003edaeae0d50ab65b4a9ab867f7fef6cafe
Aiden-Chubet <147650530+Aiden-Chubet@users.noreply.github.com>
2024-01-10 13:31:54 -0500

    Removes Specified Licensing

---
+++ AddRemoveAADLicense.ps1
@@ -0,0 +1,14 @@
+﻿Connect-Graph -Scopes User.ReadWrite.All, Organization.Read.All
+
+$UserID = 'CCaban@cmhacc.org'
+
+$EmsSku = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'ENTERPRISEPACK'
+Set-MgUserLicense -UserId $UserID -AddLicenses @() -RemoveLicenses @($EmsSku.SkuId)
+
+$EmsSku = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'ATP_ENTERPRISE'
+Set-MgUserLicense -UserId $UserID -AddLicenses @() -RemoveLicenses @($EmsSku.SkuId)
+
+$EmsSku = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'AAD_PREMIUM'
+Set-MgUserLicense -UserId $UserID -AddLicenses @() -RemoveLicenses @($EmsSku.SkuId)
+
+Get-MgUserLicenseDetail -UserId $UserID
